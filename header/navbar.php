f7e2210eb1615067273f423a4fd30a531c410b08
Kavinnandha <kavinnandhakavin@gmail.com>
2025-03-10 12:25:17 +0530

    Update navbar.php

--- header/navbar.php
+++ header/navbar.php
@@ -173,7 +173,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <span class="text-xs">Login</span>
+                <span class="text-xs">Register</span>
             </a>
         <?php else: ?>
             <a href="/kalam/user/signout.php"
