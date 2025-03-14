cdb3a6e6ffe039591874d115a894753f069575e4
Karthibanr <karthibanlink@gmail.com>
2025-03-14 18:49:22 +0530

    Update events.php
    registration_fee = 0 displaying as Free with Entry ticket

--- categories/events.php
+++ categories/events.php
@@ -155,7 +155,15 @@
                                                       d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                             </svg>
                                             <span class="text-lg font-semibold text-gray-800">
-                                                ₹<?php echo number_format($event['registration_fee'], 2); ?>
+                                                ₹<?php 
+                                                    if($event['registration_fee']==0){
+                                                        echo("Free with Entry Ticket");
+                                                    }
+                                                    else{
+                                                        // echo $event['registration_fee'];
+                                                        echo number_format($event['registration_fee'], 2);
+                                                    }
+                                                ?>
                                             </span>
                                         </div>
                                     </div>
